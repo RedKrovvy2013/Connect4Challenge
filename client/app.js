2da5be5950aa0ffa77ec8eb7167a1d5e1430ed23
mikecirone <michael@ballparq.io>
2018-04-10 22:49:01 -0700

    game engine and view components: set up a Game service for controlling games and a Connect4 service which is a 'controlled' game; also set up side interface view component and player entry form component, and associated controllers that talk to services

--- client/app.js
+++ client/app.js
@@ -6,7 +6,7 @@ app.service('Game', function() {
     this.isPreGame = true;
     this.isPlaying = false;
     this.isPostGame = false;
-    this.addGame = (game) => {
+    this.insertGame = (game) => {
         this.game = game;
     };
     this.init = (p1Name, p2Name) => {
@@ -28,7 +28,7 @@ app.service('Game', function() {
 });
 
 app.service('Connect4', function(Game) {
-    Game.addGame(this);
+    Game.insertGame(this);
     this.p1Name = '';
     this.p2Name = '';
     this.board = []; // ..becomes multidimensional array
@@ -43,10 +43,63 @@ app.service('Connect4', function(Game) {
         // TODO
     };
     this.move = (coord) => {
+        console.log("coord: " + coord);
+        console.log("activePlayer: " + this.activePlayer);
         // move is legal:
         //   - update board and switch this.activePlayer, or
+        this.activePlayer === 0 ? this.activePlayer = 1 :
+                                  this.activePlayer = 0
         //   - move is a winning move, call Game.handleGameWinner(activePlayer)
         // move is not legal:
         //   - do nothing
-    }
+    };
+});
+
+function Connect4TwoPlayerInterfaceController(Connect4) {
+    this.move = function(coord) {
+        Connect4.move(coord);
+    };
+}
+
+app.directive('twoPlayerInterface', function() {
+    return {
+        restrict: 'E',
+        scope: {},
+        templateUrl: "twoPlayerInterface.html",
+        controller: Connect4TwoPlayerInterfaceController,
+        controllerAs: '$ctrl',
+        link: function($scope, elem, attrs, ctrl) {
+            $scope.onMove = function() {
+                ctrl.move($scope.coord);
+            };
+        }
+    };
+});
+
+function twoPlayerEntryFormController($scope, Game) {
+    $scope.Game = Game;
+    $scope.$watch("Game.isPreGame", () => {
+        this.isShowForm = $scope.Game.isPreGame;
+        // auto-inits since we're not doing newVal !== oldVal check,
+        // plus makes it easier for future features where we may not want
+        // to immediately show entry form
+    });
+    this.init = function(p1Name, p2Name) {
+        Game.init(p1Name, p2Name);
+    };
+}
+
+app.directive('twoPlayerEntryForm', function() {
+    return {
+        restrict: 'E',
+        scope: {},
+        templateUrl: "twoPlayerEntryForm.html",
+        controller: twoPlayerEntryFormController,
+        controllerAs: '$ctrl',
+        link: function($scope, elem, attrs, ctrl) {
+            $scope.onSubmit = function() {
+                ctrl.init($scope.p1Name, $scope.p2Name);
+            };
+        }
+    };
 });
